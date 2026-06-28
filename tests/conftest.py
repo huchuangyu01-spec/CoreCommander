6c17dc570f0769a614f83af9a546dc8837f2f9f3
_可燃垃圾 <huchuangyu01@gmail.com>
2026-06-28 16:39:18 +0800

    Delete tests directory

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-# Ensure mock_bootstrap is imported first to patch platform-specific libraries before any other imports
-try:
-    from tests import mock_bootstrap
-except ImportError:
-    import mock_bootstrap
-
-import pytest
